3531407df2257da684060a1e20c15f6f43aad92e
Paul Cernek <ptcernek@gmail.com>
2015-06-05 13:45:25 -0700

    reduce code duplication in dcca variable picking

--- src/dcca.c
+++ src/dcca.c
@@ -2,24 +2,32 @@
 // Created by pcernek on 5/28/15.
 //
 
-#include "ubcsat.h"
 #include "dcca.h"
 
-void pickBestOldestVar(UINT32* varList, UINT32 listSize);
-
 void PickDCCA() {
-  printf("Picking a variable via DCCA\n");
 
+  pickCSDvar();
+
+  if (iFlipCandidate == 0) {
+    pickNVDvar();
+  }
+
+  if (iFlipCandidate == 0) {
+    pickSDvar();
+  }
+
+}
+
+void pickCSDvar() {
   pickBestOldestVar(csdVarsList, iNumCSDvars);
+}
 
-  if (iFlipCandidate == 0) {
-    pickBestOldestVar(nvdVarsList, iNumNVDvars);
-  }
+void pickNVDvar() {
+  pickBestOldestVar(nvdVarsList, iNumNVDvars);
+}
 
-  if (iFlipCandidate == 0) {
-    pickBestOldestVar(sdVarsList, iNumSDvars);
-  }
-
+void pickSDvar() {
+  pickBestOldestVar(sdVarsList, iNumSDvars);
 }
 
 void pickBestOldestVar(UINT32* varList, UINT32 listSize) {
@@ -28,7 +36,7 @@ void pickBestOldestVar(UINT32* varList, UINT32 listSize) {
   UINT32 bestVar = 0;
   UINT32 i;
 
-  // Find the CSD variable with the best score, breaking ties
+  // Find the variable with the best score, breaking ties
   //   in favor of the least recently-flipped variable
   for (i = 0; i < listSize; i ++) {
     iVar = varList[i];
@@ -42,66 +50,3 @@ void pickBestOldestVar(UINT32* varList, UINT32 listSize) {
 
   iFlipCandidate = bestVar;
 }
-
-//void pickCSDvar() {
-//  UINT32 iVar;
-//  SINT32 maxScore = 0;
-//  UINT32 bestCSDvar = 0;
-//  UINT32 i;
-//
-//  // Find the CSD variable with the best score, breaking ties
-//  //   in favor of the least recently-flipped variable
-//  for (i = 0; i < iNumCSDvars; i ++) {
-//    iVar = csdVarsList[i];
-//    if (aVarScore[iVar] < maxScore ||
-//        (aVarScore[iVar] == maxScore && aVarLastChange[iVar] < aVarLastChange[bestCSDvar]) )
-//    {
-//      maxScore = aVarScore[iVar];
-//      bestCSDvar = iVar;
-//    }
-//  }
-//
-//  iFlipCandidate = bestCSDvar;
-//}
-//
-//void pickNVDvar() {
-//  UINT32 iVar;
-//  SINT32 maxScore = 0;
-//  UINT32 bestNVDvar = 0;
-//  UINT32 i;
-//
-//  // Find the NVD variable with the best score, breaking ties
-//  //   in favor of the least recently-flipped variable
-//  for (i = 0; i < iNumNVDvars; i ++) {
-//    iVar = nvdVarsList[i];
-//    if (aVarScore[iVar] < maxScore ||
-//        (aVarScore[iVar] == maxScore && aVarLastChange[iVar] < aVarLastChange[bestNVDvar]) )
-//    {
-//      maxScore = aVarScore[iVar];
-//      bestNVDvar = iVar;
-//    }
-//  }
-//
-//  iFlipCandidate = bestNVDvar;
-//}
-//
-//void pickSDvar() {
-//  UINT32 iVar;
-//  SINT32 maxScore = 0;
-//  UINT32 bestSDvar = 0;
-//  UINT32 i;
-//
-//  // Find the SD variable with the best score, breaking ties
-//  //   in favor of the least recently-flipped variable
-//  for (i = 0; i < iNumNVDvars; i ++) {
-//    iVar = sdVarsList[i];
-//    if (aVarScore[iVar] < maxScore ||
-//        (aVarScore[iVar] == maxScore && aVarLastChange[iVar] < aVarLastChange[bestSDvar]) )
-//    {
-//      maxScore = aVarScore[iVar];
-//      bestSDvar = iVar;
-//    }
-//  }
-//
-//  iFlipCandidate = bestSDvar;
-//}
--- src/dcca.h
+++ src/dcca.h
@@ -5,7 +5,16 @@
 #ifndef SATENSTEIN_DCCA_H
 #define SATENSTEIN_DCCA_H
 
+#include "ubcsat.h"
+
 void PickDCCA();
+
+void pickNVDvar();
+void pickCSDvar();
+void pickSDvar();
+
+void pickBestOldestVar(UINT32* varList, UINT32 listSize);
+
 void UpdateDCCA();
 
 #endif //SATENSTEIN_DCCA_H
